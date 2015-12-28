90ab908b020c102d56233011fb044ac13612f8d9
Aref Mirhosseini <code@arefmirhosseini.com>
2015-12-28 14:53:59 +0330

    feature: block-scoping > const

---
+++ features/block-scoping/const.js
@@ -0,0 +1,23 @@
+/**
+ * const
+ * ====================================================================================================
+ * The const declaration creates a read-only reference to a value. It does not mean the value it holds 
+ * is immutable, just that the variable identifier cannot be reassigned.
+ *
+ * MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+ * RUN: node --use-strict const.js
+ */
+
+//Sample.1
+console.log("\n Sample.1");
+(function() {
+    
+    const fullname = "john doe";
+
+    console.log("fullname-1: ", fullname);
+
+    // fullname = "jenny doe"; // if set returns SyntaxError: Identifier 'fullname' has already been declared
+
+    console.log("fullname-2: ", fullname);
+
+})();
---
+++ features/block-scoping/let.js
@@ -0,0 +1,40 @@
+/**
+ * let
+ * ====================================================================================================
+ * The let statement declares a block scope local variable, optionally initializing it to a value.
+ *
+ * MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+ * RUN: node --use-strict let.js
+ */
+
+//Sample.1
+console.log("\n Sample.1");
+(function() {
+    
+    let x = 1;
+    
+    if (true) {
+    	let x = 2;
+        console.log("1:", x); //print 2
+    }
+
+    console.log("2:", x); //print 1
+
+})();
+
+//Sample.2
+console.log("\n Sample.2");
+(function(){
+
+	var a = 1,
+		b = 2;
+
+	if (a === 1) {
+		let a = 3,
+			b = 4;
+		console.log("1:", a, b);		
+	}
+
+	console.log("2:", a, b);
+
+})();
